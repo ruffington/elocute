38718ceb5c371eea9d0f038177d78a8a79115fef
Christine Tsou <cyt3ea@virginia.edu>
2017-03-26 14:27:28 -0400

    Restart button

--- Assets/Workshop/Scripts/ApplicationManager.cs
+++ Assets/Workshop/Scripts/ApplicationManager.cs
@@ -1,6 +1,7 @@
 ﻿/*SAMSUNG AND ITS AFFILIATES, SUBSIDIARIES, OFFICERS, DIRECTORS, EMPLOYEES, AGENTS, PARTNERS, AND LICENSORS (COLLECTIVELY, “SAMSUNG”) DO NOT PROMISE OR GUARANTEE THAT THE CODE CONTAINED HEREIN (INCLUDING, FUNCTIONALITY OR FEATURES OF THE FOREGOING) (COLLECTIVELY, THE “CODE”) WILL BE RELIABLE, SECURE, OR ERROR-FREE, OR THAT ANY DEFECTS WILL BE CORRECTED.  THE CODE IS PROVIDED ON AN “AS-IS” BASIS. SAMSUNG CANNOT ENSURE THAT THE CODE WILL BE FREE OF VIRUSES, CONTAMINATION OR DESTRUCTIVE FEATURES. FURTHER, SAMSUNG DOES NOT GUARANTEE ANY RESULTS OR IDENTIFICATION OR CORRECTION OF PROBLEMS AS PART OF THE CODE AND SAMSUNG DISCLAIMS ANY LIABILITY RELATED THERETO. SAMSUNG DISCLAIMS ALL WARRANTIES, EXPRESS OR IMPLIED, INCLUDING ANY WARRANTIES OF ACCURACY, NON-INFRINGEMENT, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. SAMSUNG DISCLAIMS ANY AND ALL LIABILITY FOR THE ACTS, OMISSIONS AND CONDUCT OF ANY THIRD PARTIES IN CONNECTION WITH OR RELATED TO YOUR USE OF THE CODE. YOU ASSUME TOTAL RESPONSIBILITY AND ALL RISKS FOR YOUR USE OF THE CODE. YOUR SOLE REMEDY AGAINST SAMSUNG FOR DISSATISFACTION WITH THE CODE IS TO STOP USING THE CODE. SAMSUNG HAS NO OTHER OBLIGATION OR RESPONSIBILITY TO YOU.*/
 
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using System.Collections;
 
 
@@ -18,7 +19,8 @@ public class ApplicationManager : SingletonMonoBehaviour<ApplicationManager> {
 	public string IncrementButtonTag = "IncrementButton";
 	public string DecrementButtonTag = "DecrementButton";
 	public string SlideshowButtonTag = "SlideshowButton";
-	public GameObject[] TextureObject = new GameObject[1];
+    public string RestartButtonTag = "RestartButton";
+    public GameObject[] TextureObject = new GameObject[1];
 
 	private bool mFirstTextureReady = false;
 
@@ -61,7 +63,12 @@ public class ApplicationManager : SingletonMonoBehaviour<ApplicationManager> {
             else if (mapTagToModeIndex(objHitTag) == 2) {
                 togglePause();
             }
-		}
+            else if (mapTagToModeIndex(objHitTag) == 3)
+            {
+                Debug.Log("Restart clicked");
+                Application.LoadLevel(Application.loadedLevel);
+            }
+        }
 		 
 		if(ScreenFaderSphere.getInstance() && !mFirstTextureReady && TextureLoader.getInstance().isFinishedLoadingFirstTexture()) 
 		{
@@ -85,8 +92,9 @@ public class ApplicationManager : SingletonMonoBehaviour<ApplicationManager> {
 		if(tagToMap == IncrementButtonTag) return 0;
 		if(tagToMap == DecrementButtonTag) return 1;
 		if(tagToMap == SlideshowButtonTag) return 2;
-		// Default to IncrementTag if tagToMap isn't valid button tag.
-		return 0;
+        if (tagToMap == RestartButtonTag) return 3;
+        // Default to IncrementTag if tagToMap isn't valid button tag.
+        return 0;
 	}
 
 	private bool isTagValidButton(string tagToCheck) {
